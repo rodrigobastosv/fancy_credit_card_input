400170885fe97a05beab840b7af524f268653923
Rodrigo Vasconcelos <rodrigo.vasconcelos@wendys.com>
2025-03-18 23:03:16 -0300

    Add unit tests

---
+++ test/src/entities/card_data_test.dart
@@ -0,0 +1,9 @@
+import 'package:fancy_credit_card_input/fancy_credit_card_input.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('toString', (tester) async {
+    final cardData = CardData(brand: CardBrand.visa, cardNumber: '4111111111111111', expiryMonth: 12, expiryYear: 25, cvv: '222');
+    expect(cardData.toString(), 'visa, Number: 4111111111111111, Month: 12, Year: 25, CVV: 222');
+  });
+}
---
+++ test/src/fancy_credit_card_input_test.dart
@@ -0,0 +1,204 @@
+import 'package:fancy_credit_card_input/fancy_credit_card_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Future<void> pumpFancyCreditCardInput(
+    WidgetTester tester, {
+    void Function(CardData?)? onFormCompleted,
+    required CardNumberBuilder cardNumberBuilder,
+    required DecorationBuilder decorationBuilder,
+    ErrorBuilder? errorBuilder,
+    String? cardNumberHint,
+    ExpiryDateType? expiryDateType,
+    String? Function(String?)? cardNumberValidator,
+    String? Function(String?)? expiryValidator,
+    String? Function(String?)? cvvValidator,
+  }) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: FancyCreditCardInput(
+            onFormCompleted: onFormCompleted ?? (cardData) {},
+            cardNumberBuilder: cardNumberBuilder,
+            decorationBuilder: decorationBuilder,
+            errorBuilder: errorBuilder,
+            cardNumberHint: cardNumberHint,
+            expiryDateType: expiryDateType ?? ExpiryDateType.regular,
+            cardNumberValidator: cardNumberValidator,
+            expiryValidator: expiryValidator,
+            cvvValidator: cvvValidator,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> enterCardNumber(
+    WidgetTester tester, {
+    required String cardNumber,
+  }) async {
+    final cardNumberTextField = find.byType(TextField).at(0);
+    await tester.enterText(cardNumberTextField, cardNumber);
+    await tester.pump(const Duration(milliseconds: 500));
+  }
+
+  Future<void> enterExpiry(
+    WidgetTester tester, {
+    required String expiry,
+  }) async {
+    final expiryTextField = find.byType(TextField).at(1);
+    await tester.enterText(expiryTextField, expiry);
+    await tester.pump(const Duration(milliseconds: 500));
+  }
+
+  Future<void> enterCvv(
+    WidgetTester tester, {
+    required String cvv,
+  }) async {
+    final cvvTextField = find.byType(TextField).at(2);
+    await tester.enterText(cvvTextField, cvv);
+    await tester.pump(const Duration(milliseconds: 500));
+  }
+
+  testWidgets('should show card number hint', (tester) async {
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => const SizedBox(),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      cardNumberHint: 'Enter your Card Number',
+    );
+    expect(find.text('Enter your Card Number'), findsOneWidget);
+  });
+
+  testWidgets('should execute the cardNumberBuilder after the card number field is completely filled', (tester) async {
+    var cardNumberBuilderCalled = false;
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) {
+        cardNumberBuilderCalled = true;
+        return Text(cardLastFourDigits);
+      },
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      cardNumberHint: 'Enter your Card Number',
+    );
+    await enterCardNumber(tester, cardNumber: '4111111111111234');
+    expect(find.text('1234'), findsOneWidget);
+    expect(cardNumberBuilderCalled, true);
+  });
+
+  testWidgets('should not show the last four digits if the card number is not completely filled', (tester) async {
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => Text(key: const ValueKey('last-four-digits'), cardLastFourDigits),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      cardNumberHint: 'Enter your Card Number',
+    );
+    await enterCardNumber(tester, cardNumber: '1');
+    expect(find.text('Enter your Card Number'), findsOneWidget);
+  });
+
+  testWidgets('should expand the card number again after being clicked while being colapsed', (tester) async {
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => Text(key: const ValueKey('last-four-digits'), cardLastFourDigits),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+    );
+    await enterCardNumber(tester, cardNumber: '4111111111111234');
+    await enterExpiry(tester, expiry: '1225');
+    final lastFourDigits = find.byKey(const ValueKey('last-four-digits'));
+    await tester.tap(lastFourDigits);
+    await tester.pumpAndSettle(const Duration(milliseconds: 500));
+    await tester.pump(const Duration(milliseconds: 500));
+    expect(find.text('4111 1111 1111 1234'), findsOneWidget);
+  });
+
+  testWidgets('should input the expiry correctly masking the value', (tester) async {
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => Text(cardLastFourDigits),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      cardNumberHint: 'Enter your Card Number',
+    );
+    await enterCardNumber(tester, cardNumber: '4111111111111234');
+    await enterExpiry(tester, expiry: '1225');
+    expect(find.text('12/25'), findsOneWidget);
+  });
+
+  testWidgets('should call error builder showing error when validates', (tester) async {
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => Text(cardLastFourDigits),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      errorBuilder: (errorMessage) => const Text('Error'),
+    );
+    await enterCardNumber(tester, cardNumber: '41');
+    expect(find.text('Error'), findsOneWidget);
+  });
+
+  testWidgets('should input the expiry correctly masking the value with the full year ExpiryDateType', (tester) async {
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => Text(cardLastFourDigits),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      expiryDateType: ExpiryDateType.fullYear,
+    );
+    await enterCardNumber(tester, cardNumber: '4111111111111234');
+    await enterExpiry(tester, expiry: '122025');
+    expect(find.text('12/2025'), findsOneWidget);
+  });
+
+  testWidgets('should call onFormCompleted with the correct data when all the values are inputed', (tester) async {
+    var onFormCompletedCalled = false;
+    CardData? cardData;
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => Text(cardLastFourDigits),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      onFormCompleted: (data) {
+        onFormCompletedCalled = true;
+        cardData = data;
+      },
+    );
+    await enterCardNumber(tester, cardNumber: '4111111111111234');
+    await enterExpiry(tester, expiry: '1225');
+    await enterCvv(tester, cvv: '123');
+    await tester.pumpAndSettle();
+    expect(onFormCompletedCalled, true);
+    expect(cardData?.cardNumber, '4111111111111234');
+    expect(cardData?.expiryMonth, 12);
+    expect(cardData?.expiryYear, 25);
+    expect(cardData?.cvv, '123');
+  });
+
+  testWidgets('should call all the validators when they are given', (tester) async {
+    var cardNumberValidatorCalled = false;
+    var expiryValidatorCalled = false;
+    var cvvValidatorCalled = false;
+    await pumpFancyCreditCardInput(
+      tester,
+      cardNumberBuilder: (brand, cardLastFourDigits) => Text(cardLastFourDigits),
+      decorationBuilder: (hasFocus, hasError) => const BoxDecoration(),
+      onFormCompleted: (data) {},
+      cardNumberValidator: (cardNumber) {
+        cardNumberValidatorCalled = true;
+        return null;
+      },
+      expiryValidator: (expiry) {
+        expiryValidatorCalled = true;
+        return null;
+      },
+      cvvValidator: (cvv) {
+        cvvValidatorCalled = true;
+        return null;
+      },
+    );
+    await enterCardNumber(tester, cardNumber: '4111111111111234');
+    await enterExpiry(tester, expiry: '1225');
+    await enterCvv(tester, cvv: '123');
+    await tester.pumpAndSettle();
+    expect(cardNumberValidatorCalled, true);
+    expect(expiryValidatorCalled, true);
+    expect(cvvValidatorCalled, true);
+  });
+}
